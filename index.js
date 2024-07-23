70aa15159fb43bcb50760302354d133c1bb32963
gayatrir06 <115918121+gayatrir06@users.noreply.github.com>
2024-07-23 11:00:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function cv{
+    alert("CV opened sucessfully....");
+}
+
+
+function contact{
+    alert("Thank you for contacting.....");
+}
+
+function cv(onclick);
+
+function contact(onclick);
